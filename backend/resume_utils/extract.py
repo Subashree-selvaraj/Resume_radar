3ad379f63bc80296a94bf5e5464c61346eb71b78
subashree <132484553+Subashree-selvaraj@users.noreply.github.com>
2025-04-05 16:23:47 +0530

    Delete backend directory

--- backend/resume_utils/extract.py
+++
@@ -1,8 +0,0 @@
-import fitz  # PyMuPDF
-
-def extract_text_from_pdf(file_path):
-    text = ""
-    with fitz.open(file_path) as pdf:
-        for page in pdf:
-            text += page.get_text()
-    return text
